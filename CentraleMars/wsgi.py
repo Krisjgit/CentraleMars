51d65ca3296f992ef9362e875bf3a0447f4ea220
Krisjgit <84929089+Krisjgit@users.noreply.github.com>
2023-11-11 22:19:17 +0100

    modif quasi rien

--- CentraleMars/wsgi.py
+++ CentraleMars/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+import sys
 
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CentraleMars.settings')
 
 application = get_wsgi_application()
