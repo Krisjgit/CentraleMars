dc2f04509c2ede3fee13b9035498274ebb723ec9
Krisjgit <84929089+Krisjgit@users.noreply.github.com>
2023-11-11 22:05:06 +0100

    deployement

--- CentraleMars/settings.py
+++ CentraleMars/settings.py
@@ -9,7 +9,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -19,9 +18,8 @@ SECRET_KEY = '39@30sey%z1122f20ss!dzd_4*^wctl(k+ad9w%(i_xonm#695'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','.centrale-marseille.fr','kris.joubi.perso.centrale-marseille.fr/','vercel.app']
-
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.centrale-marseille.fr',
+                 'kris.joubi.perso.centrale-marseille.fr/', '.vercel.app']
 
 # Application definition
 
@@ -65,7 +63,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CentraleMars.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -75,7 +72,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -95,7 +91,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -108,7 +103,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
